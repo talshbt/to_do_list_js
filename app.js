6672f2462467edce363b76dd9a25e3cf37c1fd9f
tal.shabtai <talshbt@gmail.com>
2019-09-25 17:46:31 +0300

    update

--- app.js
+++ app.js
@@ -11,34 +11,18 @@ $(document).ready(
                  $('#mylist').append(`<li class="list-group-item ">
                                     <div class="checkbox">
                                         <input type="checkbox" id="checkbox2" />
-                                        <label for="checkbox2">
-                                            `+toAdd+`
-                                        </label>
-
+                                        <label for="checkbox2">`+toAdd+`</label>
                                         <div class="pull-right action-buttons">
                                                 <a href="http://www.jquery2dotnet.com"><span class="glyphicon glyphicon-pencil"></span></a>
                                                 <a href="http://www.jquery2dotnet.com" class="trash"><span class="glyphicon glyphicon-trash"></span></a>
                                                 <a href="http://www.jquery2dotnet.com" class="flag"><span class="glyphicon glyphicon-flag"></span></a>
-                                            </div>
-                                    </div>
-
-                                 
-                            </li>`);
-
-
-
-                         
-
-
-
-
-
+                                        </div></div></li>`);
             });
 
 
           
        
-       $("input[name=ListItem]").keyup(function(event){
+     $("input[name=ListItem]").keyup(function(event){
           if(event.keyCode == 13){
             $("#button").click();
           }         
@@ -50,30 +34,9 @@ $(document).ready(
       
       $('input').focus(function() {
         $(this).val('');
-      });
-
-      
-      
+      }); 
     }
 );
-
-
-// $(document).ready(
-//     function(){
-
-
-//         $('#add-list-btn').click(
-//             function(){
-//                 alert("The paragraph was clicked.");
-
-//                 document.getElementById("addListInput").style.visibility = "visible";
-//                 var toAdd = $('input[name=addListInput]').val();
-//                 $('#my-menu').append(`<a href="#">`+toAdd+`</a>`);
-
-//             });
-      
-//     }
-// );
 
 $(document).ready(function(){
     $('a[href="#add-list-btn"]').click(function(){
@@ -93,14 +56,23 @@ function openNav() {
 
 
   function addList() {
-    var toAdd = $('input[name=menu-list]').val();
 
-    //  alert("The paragraph was clicked.");
      document.getElementById("addListInput").style.visibility = "visible";
-    // $('#my-menu').append(`<a href="#">`+toAdd+`</a>`);
-    console.log(toAdd);
-
-    // document.getElementById("add-list-btn").style.width = "250px";
-    // document.getElementById("main").style.marginLeft = "250px";
   }
   
+  
+ 
+  $('#addListInput').keypress(function(event){
+    var toAdd = $('input[name=menu-list]').val();
+    console.log(toAdd)
+
+    var code = event.keyCode || event.which;
+    if(code == '13'){
+        console.log("enter preessed");
+         $('#my-menu').append(`<a href="#"><i class="fa fa-book"></i>
+         <span>`+toAdd+`</span></a>`);
+         document.getElementById("addListInput").style.visibility = "hidden";
+    }
+   
+    event.stopPropagation();
+});
