4517599a024519e643808f60e58a46ad53f6de0c
tal.shabtai <talshbt@gmail.com>
2019-09-25 18:01:33 +0300

    update

--- app.js
+++ app.js
@@ -76,3 +76,21 @@ function openNav() {
    
     event.stopPropagation();
 });
+
+
+var items = document.querySelectorAll(".list-group-item");
+var tab = [], index;
+// get tje selected li index using array
+// populate array with li values
+
+for(var i = 0; i < items.length; ++i){
+    tab.push(items[i].innerHTML);
+}
+
+// get li index on click
+for(var i = 0; i < items.length; ++i){
+    items[i].onclick = function(){
+        index = tab.indexOf(this.innerHTML);
+        console.log(this.innerHTML + "index: " + index);
+    };
+}
